903697cd225a87fbebd4fa7a394258edce6af4c1
devops <rajesh.m712@gmail.com>
2019-02-28 13:17:24 +0530

    commit the mvn project

---
+++ src/main/java/devpack/WelcomeRepository.java
@@ -0,0 +1,17 @@
+package devpack;
+
+import org.eiichiro.gig.Repository;
+import org.eiichiro.jaguar.inject.Name;
+
+@Name("devpack.WelcomeRepository")
+@Repository
+public class WelcomeRepository {
+
+	public Welcome get() {
+		// It's OK to get it from database.
+		Welcome welcome = new Welcome();
+		welcome.status = "READY";
+		return welcome;
+	}
+	
+}
---
+++ src/main/java/devpack/WelcomeService.java
@@ -0,0 +1,17 @@
+package devpack;
+
+import org.eiichiro.gig.Service;
+import org.eiichiro.jaguar.inject.Inject;
+import org.eiichiro.jaguar.inject.Name;
+
+@Name("devpack.WelcomeService")
+@Service
+public class WelcomeService {
+
+	@Inject private WelcomeRepository repository;
+	
+	public Welcome get() {
+		return repository.get();
+	}
+	
+}
